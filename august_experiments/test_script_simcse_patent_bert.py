dfb8ef04dacae5670a56531fe32786688fe85bee
Luke Kim <puzzlecollector1123581321@gmail.com>
2022-08-25 00:20:35 +0900

    Update and rename test_script_dcpcse.py to test_script_simcse_patent_bert.py

--- august_experiments/test_script_simcse_patent_bert.py
+++ august_experiments/test_script_simcse_patent_bert.py
@@ -21,44 +21,41 @@ test_labels = test_df['라벨링'].values
 test_correlations, test_accuracies = [], [] 
 
 for i in range(1, 11): 
-    model_idx = i 
     print("loading model {}...".format(i))  
-    model = SentenceTransformer("../storage/dcpcse_patent_for_bert_KFOLD{}".format(model_idx)) 
+    model = SentenceTransformer("../storage/simcse_patent_for_bert_KFOLD{}".format(i)) 
     model.max_seq_length = 128 
     eval_s1, eval_s2, eval_scores = [], [], [] 
-    for i in range(len(test_queries)): 
-        eval_s1.append(test_queries[i]) 
-        eval_s2.append(test_candidates[i]) 
-        eval_scores.append(test_labels[i]) 
+    for j in range(len(test_queries)): 
+        eval_s1.append(test_queries[j]) 
+        eval_s2.append(test_candidates[j]) 
+        eval_scores.append(test_labels[j]) 
     evaluator = evaluation.EmbeddingSimilarityEvaluator(eval_s1, eval_s2, eval_scores) 
     test_corr = model.evaluate(evaluator) 
     test_correlations.append(test_corr) 
     
     print("calculating accuracy") 
     cnt = 0 
-    for i in tqdm(range(len(test_queries))): 
-        q_emb = model.encode(test_queries[i]) 
-        c_emb = model.encode(test_candidates[i]) 
+    for j in tqdm(range(len(test_queries))): 
+        q_emb = model.encode(test_queries[j]) 
+        c_emb = model.encode(test_candidates[j]) 
         cosine_scores = util.cos_sim(q_emb, c_emb) 
-        if test_labels[i] == 0 and cosine_scores < 0.5: 
+        if test_labels[j] == 0 and cosine_scores < 0.5: 
             cnt += 1 
-        elif test_labels[i] == 0.5 and (cosine_scores >= 0.5 and cosine_scores < 0.8): 
+        elif test_labels[j] == 0.5 and (cosine_scores >= 0.5 and cosine_scores < 0.8): 
             cnt += 1 
-        elif test_labels[i] >= 0.8 and cosine_scores >= 0.8: 
+        elif test_labels[j] >= 0.8 and cosine_scores >= 0.8: 
             cnt += 1 
     test_accuracy = cnt / len(test_labels) * 100.0 
     test_accuracies.append(test_accuracy) 
     
     print(test_corr, test_accuracy) 
-    
 
-    
 print(np.mean(test_correlations)) 
 print(np.mean(test_accuracies)) 
 
 
-with open("dcpcse_patent_for_bert_test_correlations", "wb") as fp:   #Pickling
+with open("simcse_patent_for_bert_test_correlations", "wb") as fp:   #Pickling
     pickle.dump(test_correlations, fp)
     
-with open("dcpcse_patent_for_bert_test_accuracies", "wb") as fp:   #Pickling
+with open("simcse_patent_for_bert_test_accuracies", "wb") as fp:   #Pickling
     pickle.dump(test_accuracies, fp)
